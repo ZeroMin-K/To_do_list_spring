90a6e022f6c78bc8bcd187e032a4ed9008508d7c
ZeroMin-K <ymkim9510@gmail.com>
2022-07-27 00:26:03 +0900

    Controller 실습
    Controller 매개변수 받는 방법 실습
    TestRequestBodyDTO 생성하여 ReqeustBodyDTO 받는 실습 진행

--- src/main/java/com/example/todolist_spring/controller/TestController.java
+++ src/main/java/com/example/todolist_spring/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.example.todolist_spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.todolist_spring.dto.ResponseDTO;
+import com.example.todolist_spring.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")     // 리소스
@@ -15,5 +19,39 @@ public class TestController {
     @GetMapping("/testGetMapping")
     public String testControllerWithPath() {
         return "Hello World! testGetMapping";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required=false) int id)
+    {
+        return "Hello World! ID: " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! ID: " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! ID " + testRequestBodyDTO.getId() +
+                " Message : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World I'm ResponseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity. you got 400");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        // http status를 400 으로 설정
+        return ResponseEntity.badRequest().body(response);
     }
 }
